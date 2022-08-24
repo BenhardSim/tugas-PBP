e8076db64822d899e5e25defb703901c852cc160
BenhardSim <benhard.simanullang.bs@gmail.com>
2022-08-24 18:20:05 +0700

    add new style

--- main.js
+++ main.js
@@ -1,17 +1,32 @@
-function addActivity(){
+function remElmt(el){
+    var element = el;
+    element.remove();
+}
+
+function addActivity() {
     // mengambil nilai yang ditulis dalam tag input
     var val = document.getElementById("kegiatan").value;
 
-    // membuat element list baru
-    var newList = document.createElement("li");
+    if (val === '') {
+        document.getElementById("warning").style.visibility = "visible";
+    } else {
+        // membuat element list baru
+        var newList = document.createElement("li");
 
-    // membuat isi dari list element baru dengan nilai yang ditulis
-    // dalam element input
-    newList.innerHTML = val;
+        // membuat isi dari list element baru dengan nilai yang ditulis
+        // dalam element input
+        newList.innerHTML = val;
+        newList.onclick = remElmt;
 
-    // memasukkan element baru yang dibuat kedalam element dengan ID list-action
-    document.getElementById("list-action").appendChild(newList);
+        // memasukkan element baru yang dibuat kedalam element dengan ID list-action
+        document.getElementById("list-action").appendChild(newList);
 
-    // menghilangkan text pada tag input
-    document.getElementById("kegiatan").value = '';    
+        // menghilangkan text pada tag input
+        document.getElementById("kegiatan").value = '';
+
+        // menghilangkan warning
+        document.getElementById("warning").style.visibility = "hidden";
+    }
+
+
 }
